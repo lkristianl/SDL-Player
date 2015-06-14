3ca291c2fd070ef370223af7fefef65d234b31b9
BigPox <jozigila@gmail.com>
2015-06-14 17:10:44 +0200

    Added event handle and FPS regulator
    Removed the "play" function (for now), added "mainHandle" which also
    includes the event handle.
    TO-DO: Turn "mainHandle" into a GUI-handling wrapper for the play
    function.

--- SDL Videoplayer/Source.cpp
+++ SDL Videoplayer/Source.cpp
@@ -6,39 +6,63 @@
 //Prototypes
 bool init();
 bool loadFrames(int);
-void play(int);
+int	 mainHandle(int);
 void close();
 
 //Global vars
 const int SCR_W = 500;
 const int SCR_H = 282;
 
+//Create the display window container, windows screen, and the buffer (vector).
 SDL_Window *gWindow = NULL;
 SDL_Surface *gScreenSurface = NULL;
 std::vector<SDL_Surface *> gFrames;
 
 int main(int argc, char* argv[]){
 	int frames = 10;
-
+	
 	init();
 	loadFrames(frames);
-	play(frames);
+	mainHandle(frames);
+	close();
 
 	system("PAUSE");
 	return 0;
 }
 
+//The main handle for the window.
+//Handles user events and display frames.
+int mainHandle(int frames){
+	int currentFrame = 0;
+
+	SDL_Event e;
+	while (true){
+		while (SDL_PollEvent(&e) != 0)
+		{
+			//User requests quit
+			if (e.type == SDL_QUIT) return 0;
+		}
+
+		//Display frame
+		SDL_BlitSurface(gFrames[currentFrame], NULL, gScreenSurface, NULL);
+		SDL_UpdateWindowSurface(gWindow);
+		currentFrame++;
+		if (currentFrame > frames - 1) currentFrame = 0; //Loop frames
+		SDL_Delay(1000 / 30); //Regulate FPS
+	}
+}
+
+//Initialize SDL video and create window.
+//Returns true if successfully initialized.
 bool init(){
-	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) == -1){
-		printf("Could not initialize SDL! \nSDL error: %s\n", SDL_GetError());
+		printf("SDL error: %s\n", SDL_GetError());
 		return false;
 	}
 	else{
-		//Create window
 		gWindow = SDL_CreateWindow("SDL Window", 200, 200, SCR_W, SCR_H, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL){
-			printf("Could not create window! \nSDL error: %s\n", SDL_GetError());
+			printf("SDL error: %s\n", SDL_GetError());
 			return false;
 		}
 		else{
@@ -48,6 +72,8 @@ bool init(){
 	return true;
 }
 
+//Load the BMP images in memory, frames are held as pointers to the SDL_Surface that contains them.
+//Returns true if successfully loaded in memory.
 bool loadFrames(int frames){
 	for (int i = 0; i < frames; i++){
 		std::stringstream file;
@@ -61,17 +87,7 @@ bool loadFrames(int frames){
 	return true;
 }
 
-void play(int frames){
-	int currentFrame = 0;
-	do{
-		SDL_BlitSurface(gFrames[currentFrame], NULL, gScreenSurface, NULL);
-		SDL_UpdateWindowSurface(gWindow);
-		currentFrame++;
-		if (currentFrame > frames - 1) currentFrame = 0;
-	} while (1);
-
-}
-
+//Destroys everything.
 void close(){
 	for (int i = 0; i < gFrames.size(); i++){
 		SDL_FreeSurface(gFrames[i]);
